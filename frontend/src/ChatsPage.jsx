8a4ec4f800d41448421988a76eb590229d97c2cb
EMRE YAZICI <emre@EMREs-MacBook-Pro.local>
2023-10-26 15:37:13 +0300

    pretty-version

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,20 +1,14 @@
-import {
-  MultiChatSocket,
-  MultiChatWindow,
-  useMultiChatLogic,
-} from "react-chat-engine-advanced";
+import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 const ChatsPage = (props) => {
-  const chatProps = useMultiChatLogic(
-    `
-    5b66b5b3-9149-4526-8385-c8de176e732b`,
-    props.user.username,
-    props.user.secret
-  );
   return (
     <div style={{ height: "100vh" }}>
-      <MultiChatSocket {...chatProps} />
-      <MultiChatWindow style={{ height: "100%" }} />
+      <PrettyChatWindow
+        projectId="5b66b5b3-9149-4526-8385-c8de176e732b"
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
     </div>
   );
 };
